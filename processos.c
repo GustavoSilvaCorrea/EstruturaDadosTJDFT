87752782e8e21e997405969efed22c068c33d0fe
GustavoSilvaCorrea <gusta0581476@gmai.com>
2025-09-06 13:25:00 -0300

    Declaração das funções 3/12

---
+++ processos.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "processos.h"
+
+#include <stdio.h>
+
+FILE *LerDados(const char *arquivo) {
+    FILE *fp = fopen(arquivo, "r");
+    if (!fp) {
+        perror("Erro ao abrir arquivo");
+        return NULL; // retorna NULL se não conseguiu abrir
+    }
+    
+    return fp; // retorna o ponteiro do arquivo
+}
+
+int numeroProcessos(char *arquivo)
+{
+    FILE *fp = LerDados(arquivo);
+
+    int i = 0;
+    char linha[2048]; // tamanho que uma linha pode ter em caracteres
+    Processos p;
+    fgets(linha, sizeof(linha), fp); // Pula o cabeçalho.
+
+    while (fgets(linha, sizeof(linha), fp))
+    { // "While" fgets for verdadeiro (ele é verdadeiro até passar por todas as linhas)
+        if (sscanf(linha, "%d;", &p.id_processo) == 1)
+        {        // Lê apenas o id_processos de cada linha
+            i++; // A cada id processo soma um
+        }
+    }
+
+    fclose(fp);
+    return i; // retorna o resultado
+}
+
+int id_ultimo_oj(char *arquivo, int id_processo)
+{
+    FILE *fp = LerDados(arquivo);
+
+    char linha[2048];
+    Processos p;
+    fgets(linha, sizeof(linha), fp);
+
+    while (fgets(linha, sizeof(linha), fp))
+    {
+        if (sscanf(linha, "%d;%*[^;];%*[^;];%*[^;];%*[^;];%*[^;];%*[^;];%*[^;];%d;",
+                   &p.id_processo, &p.id_ultimo_oj) == 2)
+        {
+            if (p.id_processo == id_processo)
+            {
+                return p.id_ultimo_oj;
+            }
+        }
+    }
+    fclose(fp);
+    return -1;
+}
+
+int processoAntigo(char *arquivo)
+{
+    FILE *fp = LerDados(arquivo);
+
+    char linha[2048];
+    Processos p;
+    fgets(linha, sizeof(linha), fp);
+    char data[11] = "9999-12-31"; // valor inicial bem "grande"
+    int antigo = 0;
+
+    while (fgets(linha, sizeof(linha), fp))
+    {
+        while (fgets(linha, sizeof(linha), fp))
+        {
+            if (sscanf(linha,
+                       "%d;%*[^;];%*[^;];%*[^;];%*[^;];%*[^;];%*[^;];%*[^;];%*[^;];%10[^;];",
+                       &p.id_processo, p.dt_recebimento) == 2) // Pega o id do processo e a data de recebimento.
+            {
+
+                if (strcmp(p.dt_recebimento, data) < 0) // se a data atual for mais antiga que a armazenada.
+                {
+                    strcpy(data, p.dt_recebimento); // atualiza a data mais antiga.
+                    antigo = p.id_processo; // guarda o id do processo correspondente.
+                }
+            }
+        }
+    }
+    fclose(fp);
+    return antigo;
+}
